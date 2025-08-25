e8b313849bfbc5d55c4b6f70ece65cfdd4dae850
swiftrx <swiftrx@yandex.ru>
2025-08-25 17:25:17 +0300

    initialization project 2D Clicker

---
+++ 2D_Clicker_Knights/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System;
+using Interfaces;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputActionAsset InputActions;
+    
+    private InputAction attackAction;
+    private GameObject currentEnemy;
+    private Animator currentEnemyAnimator;
+    [SerializeField] private TMP_Text TXT_Score;
+    [SerializeField] private TMP_Text TXT_EnemyName;
+    
+    [SerializeField] private float StartDamage;
+    private IKnightEnemy _currentEnemyInfo;
+
+    private void Awake()
+    {
+        attackAction = InputActions.FindActionMap("Player").FindAction("Attack");
+    }
+
+    private void Start()
+    {
+        _currentEnemyInfo = new MouseEnemy();
+        TXT_EnemyName.SetText(_currentEnemyInfo.GetName());
+        currentEnemy = GameObject.FindGameObjectWithTag("Enemy");
+        currentEnemyAnimator = currentEnemy.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (attackAction.WasPressedThisFrame())
+        {
+            int score = int.Parse(TXT_Score.GetParsedText());
+            TXT_Score.SetText((score+StartDamage).ToString());
+            currentEnemyAnimator.SetTrigger("Hurt");
+        }
+    }
+}
